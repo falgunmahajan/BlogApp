f3946825cb1ba0bca6b5b445c9fdb20021b4994e
falgunmahajan <falgunmahajan1999@gmail.com>
2024-04-07 20:45:00 +0530

    validation

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { isMatchPassword, isValidContact, isValidData, isValidEmail, isValidFullName, isValidPassword } from '@/utils/validation/validate'
 import { Call, Lock, Mail, Person, Visibility, VisibilityOff } from '@mui/icons-material'
 import axios from 'axios'
 import Link from 'next/link'
@@ -24,12 +25,29 @@ const page = () => {
     }
     }
   const [state, dispatch] = useReducer(reducer, initialState)
+  const [boolMsg,msg]=isValidData(state);
+  const [err,setErr]=useState({
+   fullNameError:"",
+   emailError:"",
+   contactError:"",
+   passwordError:"",
+   confirmPasswordError:""
+  })
   const [visiblity,setVisibility]=useState(false)
   const handleSubmit=async(e:SyntheticEvent)=>{
+    console.log(boolMsg,msg);
+    
     e.preventDefault()
+    if(boolMsg){
+
+      const res = await axios.post(`/api/auth/signup`,state)
+    }
+  else console.log(msg);
+  
     
     
- const res = await axios.post(`/api/auth/signup`,state)
+    
+
   }
   return (
     <div>
@@ -37,74 +55,120 @@ const page = () => {
       <div className="lg:w-1/4 sm:w-1/2   shadow-2xl shadow-transparent/50 p-5 rounded-md">
         <h1 className="text-3xl m-8 text-center text-pink-500 font-bold">Sign Up</h1>
         <form onSubmit={handleSubmit}>
-        <div className="relative m-6">
+          <div className='m-6'>
+        <div className="relative ">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <Person color="disabled" />
           </div>
+          
           <input
             type="text"
             id="input-group-1"
-            className="border text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 "
+            className={`border ${!err.fullNameError?"":"border-red-500"} text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 `}
             name="fullName"
             placeholder="Full Name"
+            onBlur={(e)=>{ 
+              if(!isValidFullName(e.target.value)) setErr({...err,fullNameError:"Please enter a valid fullname!"})
+              else setErr({...err,fullNameError:""})
+            }}
             onChange={(e)=>dispatch({type:"change",key:e.target.name, value:e.target.value})}
           />
+          </div>
+          <p className='  text-red-500 text-xs'>{err.fullNameError}</p>
+          
+         
         </div>
-        <div className="relative m-6">
+        <div className='m-6'>
+        <div className="relative ">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <Mail color="disabled" />
           </div>
           <input
             type="text"
             id="input-group-1"
-            className="border text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 "
+            className={`border ${!err.emailError?"":"border-red-500"} text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 `}
             name="email"
             placeholder="Email Address"
+            onBlur={(e)=>{ 
+              if(!isValidEmail(e.target.value)) setErr({...err,emailError:"Please enter a valid email!"})
+              else setErr({...err,emailError:""})
+            }}
             onChange={(e)=>dispatch({type:"change",key:e.target.name, value:e.target.value})}
           />
         </div>
-        <div className="relative m-6">
+          <p className='  text-red-500 text-xs'>{err.emailError}</p>
+          
+        </div>
+        <div className='m-6'>
+        <div className="relative ">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <Call color="disabled" />
           </div>
           <input
             type="text"
             id="input-group-1"
-            className="border text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 "
+            className={`border ${!err.contactError?"":"border-red-500"} text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5` }
             name="mobileNumber"
             placeholder="Mobile Number"
+            onBlur={(e)=>{ 
+              if(!isValidContact(e.target.value)) setErr({...err,contactError:"Please enter a valid phonenumber!"})
+              else setErr({...err,contactError:""})
+            }}
             onChange={(e)=>dispatch({type:"change",key:e.target.name, value:e.target.value})}
           />
         </div>
-        <div className="relative m-6">
+          <p className='  text-red-500 text-xs'>{err.contactError}</p>
+          
+        </div>
+        <div className='m-6'>
+        <div className="relative">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <Lock color="disabled" />
           </div>
           <input
             type={visiblity?"text":"password"}
             id="input-group-1"
-            className="border text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 "
+            className={`border ${!err.passwordError?"":"border-red-500"} text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 `}
             name="password"
             placeholder="Password"
+            onBlur={(e)=>{ 
+              if(!isValidPassword(e.target.value)) setErr({...err,passwordError:"Password must contain atleast 1 uppercase letter, 1 lowercase letter, 1 digit, 1 special character and must be of length 8!"})
+              else setErr({...err,passwordError:""})
+            }}
             onChange={(e)=>dispatch({type:"change",key:e.target.name, value:e.target.value})}
           />
           <div className="absolute inset-y-0 end-0 flex items-center pe-3.5 " onClick={()=>setVisibility(!visiblity)}>
            {visiblity?<VisibilityOff color="disabled"/>:<Visibility color="disabled"/>}
           </div>
         </div>
-        <div className="relative m-6">
+          <p className='  text-red-500 text-xs'>{err.passwordError}</p>
+          
+        </div>
+      
+        <div className='m-6'>
+        <div className="relative ">
           <div className="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
             <Lock color="disabled" />
           </div>
           <input
             type="password"
             id="input-group-1"
-            className="border text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 "
+            className={`border ${!err.confirmPasswordError?"":"border-red-500"} text-sm rounded-lg shadow shadow-transparent/40 block w-full ps-12 p-2.5 `}
             name="confirmPassword"
             placeholder="Confirm Password"
+            onBlur={(e)=>{ 
+              if(!isMatchPassword(state.password,e.target.value)) setErr({...err,confirmPasswordError:"Password and confirm password must be same!"})
+              else setErr({...err,confirmPasswordError:""})
+            }}
             onChange={(e)=>dispatch({type:"change",key:e.target.name, value:e.target.value})}
           />
         </div>
+          <p className='  text-red-500 text-xs'>{err.confirmPasswordError}</p>
+          
+        </div>
+      
+        
+       
         <div className="m-6">
           <button className="border rounded-lg w-full p-2.5 bg-pink-500 text-white shadow shadow-transparent/20 ">
            Sign Up
--- src/utils/validation/validate.ts
+++ src/utils/validation/validate.ts
@@ -1,5 +1,5 @@
 export const isValidFullName=(name:string)=>{
- if((/^[a-z]+$/).test(name))return true;
+ if((/^[a-z A-Z]+$/).test(name))return true;
  return false
 }
 
@@ -14,10 +14,34 @@ export const isValidEmail=(email:string)=>{
 }
 
 export const isValidPassword=(pswd:string)=>{
-    if(pswd.length!==8) return false
+    if(pswd.length<8) return false;
     if(!(/[0-9]/.test(pswd)))return false;
     if(!(/[a-z]/.test(pswd)))return false;
     if(!(/[A-Z]/.test(pswd)))return false;
     if(!(/\W/.test(pswd)))return false;
     return true
+}
+
+export const isMatchPassword=(pswd:String,cpswd:String)=>{
+   if(pswd===cpswd) return true;
+   return false;
+
+}
+
+export const isValidData = (data:{
+    fullName:"",
+    email:"",
+    mobileNumber:"",
+    password:"",
+    confirmPassword:"",
+    role:""
+  })=>{
+    const errObj:any={}
+    if(!isValidFullName(data.fullName)) errObj.fullNameErr="Fullname must contain only letters"
+    if(!isValidContact(data.mobileNumber)) errObj.contactErr="Phonenumber must contain only 10 digits"
+    if(!isValidEmail(data.email)) errObj.emailErr="Email is not Valid"
+    if(!isValidPassword(data.password)) errObj.pswdErr="Password must be of length 8 and contain atleast 1 uppercase letter, 1 lowercase letter, 1 digit, 1 special character"
+    if(!isMatchPassword(data.password, data.confirmPassword)) errObj.cPswdErr="Password and confirm password must be same"
+    if(Object.keys(errObj).length) return [false, errObj]
+    return [true,"All are valid"]
 }
